eb3d2edc9172067ba729234b6990e01dc65c7165
Robson Junior <rob.santanaju@gmail.com>
2023-02-07 21:55:54 -0300

    add Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
- 
+function ToggleMode()
+{
+    //switch mode
+    const html = document.documentElement
+    html.classList.toggle("Light")
+
+    //seleted profile img
+    const img = document.querySelector("#profile img")
+
+     //apply the img according the mode
+    if (html.classList.contains("Light")) {
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./assets/Avatar-Light.png")
+    } else {
+        // set tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/Avatar-Dark.png")
+    }
+}
